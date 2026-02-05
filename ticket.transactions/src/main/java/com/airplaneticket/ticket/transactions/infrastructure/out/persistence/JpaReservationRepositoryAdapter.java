daa455f86342d2b869ce651f03eca42154dcfaf2
DESKTOP-PDJSF9M\Admin <sammy.munoz@ias.com.co>
2026-02-05 16:04:56 -0500

    Se implementa el Adapter para implementar la interfaz de dominio y aspi corregir el error de bean not found

--- ticket.transactions/src/main/java/com/airplaneticket/ticket/transactions/infrastructure/out/persistence/JpaReservationRepositoryAdapter.java
+++ ticket.transactions/src/main/java/com/airplaneticket/ticket/transactions/infrastructure/out/persistence/JpaReservationRepositoryAdapter.java
@@ -3,13 +3,16 @@ package com.airplaneticket.ticket.transactions.infrastructure.out.persistence;
 import com.airplaneticket.ticket.transactions.domain.model.Reservation;
 import com.airplaneticket.ticket.transactions.domain.port.ReservationRepository;
 import com.airplaneticket.ticket.transactions.infrastructure.entity.ReservationEntity;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
+@Component("reservationRepositoryAdapter")
 public class JpaReservationRepositoryAdapter  implements ReservationRepository {
 
     private final SpringDataReservationRepository jpaRepository;
+
     public JpaReservationRepositoryAdapter(SpringDataReservationRepository jpaRepository) {
         this.jpaRepository = jpaRepository;
     }
@@ -19,7 +22,7 @@ public class JpaReservationRepositoryAdapter  implements ReservationRepository {
         ReservationEntity entity = new ReservationEntity();
         entity.setIdReservation(reservation.getIdReservation());
         entity.setUserEntity(reservation.getUser().getIdUser());
-        return null;
+        return Mono.just(reservation);
     }
 
     @Override
